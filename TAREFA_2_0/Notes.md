d339e297c4eecaab37b7c0982205baaebe8908e1
taniomi <131834608+taniomi@users.noreply.github.com>
2023-10-25 16:50:34 -0300

    [ADD] ANN.ipynb Notes.md ml_utils.py viz_ptron_relu.png
    Build perceptron relu and sigmoid

---
+++ TAREFA_2_0/Notes.md
@@ -0,0 +1,42 @@
+# Annotations
+
+### Modeling
+1. How many layers? How is the model affected by too many layers?
+    The right answer is “try different variants and see what works”. I usually work with 2 Dense hidden layers with Dropout, a technique
+that reduces overfitting by randomly setting inputs to 0. Hidden layers are useful to overcome the non-linearity of data, so if you don’t need non- linearity then you can avoid hidden layers. Too many hidden layers will lead to overfitting.
+
+2. How many neurons?
+    The number of hidden neurons should be between the size of the input layer and the size of the output layer. My rule of thumb is (number of inputs + 1 output)/2.
+
+3. What activation function?
+    There are many and we can’t say that one is absolutely better. Anyway, the most used one is ReLU, a piecewise linear function that returns the output only if it’s positive, and it is mainly used for hidden layers. Besides, the output layer must have an activation compatible with the expected output. For example, the linear function is suited for regression problems while the Sigmoid is frequently used for classification.
+
+4. How much data should be for training and how much for validation?
+
+5. What should be the batch size?
+
+6. Which loss function? (cost)
+
+7. How to optimize? (optimize = adjust)
+    Through backpropagation, but which function defines the backpropagation? How is the gradient descent done?
+
+8. What metrics? (what are metrics?)
+
+### Activation Function
+[Keras Activations - Tensor Flow DOCS](https://www.tensorflow.org/api_docs/python/tf/keras/activations)
+[Activation Function - Wikipedia](https://en.wikipedia.org/wiki/Activation_function)
+
+### Loss
+[Keras Losses - Tensor Flow DOCS](https://www.tensorflow.org/api_docs/python/tf/keras/losses)
+
+### Batch sizes
+The backpropagation and the consequent parameters update happen every batch. An
+epoch is one pass over the full training set. So, if you have 100 observations and
+the batch size is 20, it will take 5 batches to complete 1 epoch. The batch size
+should be a multiple of 2 (common: 32, 64, 128, 256) because computers usually
+organize the memory in power of 2. I tend to start with 100 epochs with a batch
+size of 32.
+
+Moreover, it’s good practice to keep a portion of the data (20%-30%) for validation. 
+In other words, the model will set apart this fraction of data to evaluate the loss 
+and metrics at the end of each epoch, outside the training.
---
+++ TAREFA_2_0/ml_utils.py
@@ -0,0 +1,237 @@
+# from DiPietro's repo
+#https://github.com/mdipietro09/DataScience_ArtificialIntelligence_Utils/blob/master/machine_learning/ml_utils.py
+
+## for data
+import numpy as np
+import pandas as pd
+
+## for plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import itertools
+
+## for deep learning
+from tensorflow.keras import models, layers, backend as K
+#import minisom
+
+## for explainer
+#from lime import lime_tabular
+import shap
+
+
+
+###############################################################################
+#                       DATA ANALYSIS                                         #
+###############################################################################
+'''
+Recognize whether a column is numerical or categorical.
+:parameter
+    :param dtf: dataframe - input data
+    :param col: str - name of the column to analyze
+    :param max_cat: num - max number of unique values to recognize a column as categorical
+:return
+    "cat" if the column is categorical, "dt" if datetime, "num" otherwise
+'''
+def utils_recognize_type(dtf, col, max_cat=20):
+    if (dtf[col].dtype == "O") | (dtf[col].nunique() < max_cat):
+        return "cat"
+    elif dtf[col].dtype in ['datetime64[ns]','<M8[ns]']:
+        return "dt"
+    else:
+        return "num"
+
+
+
+'''
+Get a general overview of a dataframe.
+:parameter
+    :param dtf: dataframe - input data
+    :param max_cat: num - mininum number of recognize column type
+'''
+def dtf_overview(dtf, max_cat=20, figsize=(10,5)):
+    ## recognize column type
+    dic_cols = {col:utils_recognize_type(dtf, col, max_cat=max_cat) for col in dtf.columns}
+        
+    ## print info
+    len_dtf = len(dtf)
+    print("Shape:", dtf.shape)
+    print("-----------------")
+    for col in dtf.columns:
+        info = col+" --> Type:"+dic_cols[col]
+        info = info+" | Nas: "+str(dtf[col].isna().sum())+"("+str(int(dtf[col].isna().mean()*100))+"%)"
+        if dic_cols[col] == "cat":
+            info = info+" | Categories: "+str(dtf[col].nunique())
+        elif dic_cols[col] == "dt":
+            info = info+" | Range: "+"({x})-({y})".format(x=str(dtf[col].min()), y=str(dtf[col].max()))
+        else:
+            info = info+" | Min-Max: "+"({x})-({y})".format(x=str(int(dtf[col].min())), y=str(int(dtf[col].max())))
+        if dtf[col].nunique() == len_dtf:
+            info = info+" | Possible PK"
+        print(info)
+                
+    ## plot heatmap
+    fig, ax = plt.subplots(figsize=figsize)
+    heatmap = dtf.isnull()
+    for k,v in dic_cols.items():
+        if v == "num":
+            heatmap[k] = heatmap[k].apply(lambda x: 0.5 if x is False else 1)
+        else:
+            heatmap[k] = heatmap[k].apply(lambda x: 0 if x is False else 1)
+    sns.heatmap(heatmap, vmin=0, vmax=1, cbar=False, ax=ax).set_title('Dataset Overview')
+    #plt.setp(plt.xticks()[1], rotation=0)
+    plt.show()
+    
+    ## add legend
+    print("\033[1;37;40m Categorical \033[m", "\033[1;30;41m Numerical/DateTime \033[m", "\033[1;30;47m NaN \033[m")
+
+
+
+###############################################################################
+#                       PREPROCESSING                                         #
+###############################################################################
+'''
+Split the dataframe into train / test
+'''
+def dtf_partitioning(dtf, y, test_size=0.3, shuffle=False):
+    dtf_train, dtf_test = model_selection.train_test_split(dtf, test_size=test_size, shuffle=shuffle) 
+    print("X_train shape:", dtf_train.drop(y, axis=1).shape, "| X_test shape:", dtf_test.drop(y, axis=1).shape)
+    print("y_train mean:", round(np.mean(dtf_train[y]),2), "| y_test mean:", round(np.mean(dtf_test[y]),2))
+    print(dtf_train.shape[1], "features:", dtf_train.drop(y, axis=1).columns.to_list())
+    return dtf_train, dtf_test
+
+
+
+###############################################################################
+#                       EXPLAINABILITY                                        #
+###############################################################################
+'''
+Use shap to build an a explainer.
+:parameter
+    :param model: model instance (after fitting)
+    :param X_names: list
+    :param X_instance: array of size n x 1 (n,)
+    :param X_train: array - if None the model is simple machine learning, if not None then it's a deep learning model
+    :param task: string - "classification", "regression"
+    :param top: num - top features to display
+:return
+    dtf with explanations
+'''
+def explainer_shap(model, X_names, X_instance, X_train=None, task="classification", top=10):
+    ## create explainer
+    ### machine learning
+    if X_train is None:
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(X_instance)
+    ### deep learning
+    else:
+        explainer = shap.DeepExplainer(model, data=X_train[:100])
+        shap_values = explainer.shap_values(X_instance.reshape(1,-1))[0].reshape(-1)
+
+    ## plot
+    ### classification
+    if task == "classification":
+        shap.decision_plot(explainer.expected_value, shap_values, link='logit', feature_order='importance',
+                           features=X_instance, feature_names=X_names, feature_display_range=slice(-1,-top-1,-1))
+    ### regression
+    else:
+        shap.waterfall_plot(explainer.expected_value[0], shap_values, 
+                            features=X_instance, feature_names=X_names, max_display=top)
+
+
+
+###############################################################################
+#                     VISUALIZE MODELS                                        #
+###############################################################################
+'''
+Extract info for each layer in a keras model.
+'''
+def utils_nn_config(model):
+    lst_layers = []
+    if "Sequential" in str(model): #-> Sequential doesn't show the input layer
+        layer = model.layers[0]
+        lst_layers.append({"name":"input", "in":int(layer.input.shape[-1]), "neurons":0, 
+                           "out":int(layer.input.shape[-1]), "activation":None,
+                           "params":0, "bias":0})
+    for layer in model.layers:
+        try:
+            dic_layer = {"name":layer.name, "in":int(layer.input.shape[-1]), "neurons":layer.units, 
+                         "out":int(layer.output.shape[-1]), "activation":layer.get_config()["activation"],
+                         "params":layer.get_weights()[0], "bias":layer.get_weights()[1]}
+        except:
+            dic_layer = {"name":layer.name, "in":int(layer.input.shape[-1]), "neurons":0, 
+                         "out":int(layer.output.shape[-1]), "activation":None,
+                         "params":0, "bias":0}
+        lst_layers.append(dic_layer)
+    return lst_layers
+
+
+
+'''
+Plot the structure of a keras neural network.
+'''
+def visualize_nn(model, description=False, figsize=(10,8)):
+    ## get layers info
+    lst_layers = utils_nn_config(model)
+    layer_sizes = [layer["out"] for layer in lst_layers]
+    
+    ## fig setup
+    fig = plt.figure(figsize=figsize)
+    ax = fig.gca()
+    ax.set(title=model.name)
+    ax.axis('off')
+    left, right, bottom, top = 0.1, 0.9, 0.1, 0.9
+    x_space = (right-left) / float(len(layer_sizes)-1)
+    y_space = (top-bottom) / float(max(layer_sizes))
+    p = 0.025
+    
+    ## nodes
+    for i,n in enumerate(layer_sizes):
+        top_on_layer = y_space*(n-1)/2.0 + (top+bottom)/2.0
+        layer = lst_layers[i]
+        color = "green" if i in [0, len(layer_sizes)-1] else "blue"
+        color = "red" if (layer['neurons'] == 0) and (i > 0) else color
+        
+        ### add description
+        if (description is True):
+            d = i if i == 0 else i-0.5
+            if layer['activation'] is None:
+                plt.text(x=left+d*x_space, y=top, fontsize=10, color=color, s=layer["name"].upper())
+            else:
+                plt.text(x=left+d*x_space, y=top, fontsize=10, color=color, s=layer["name"].upper())
+                plt.text(x=left+d*x_space, y=top-p, fontsize=10, color=color, s=layer['activation']+" (")
+                plt.text(x=left+d*x_space, y=top-2*p, fontsize=10, color=color, s="Σ"+str(layer['in'])+"[X*w]+b")
+                out = " Y"  if i == len(layer_sizes)-1 else " out"
+                plt.text(x=left+d*x_space, y=top-3*p, fontsize=10, color=color, s=") = "+str(layer['neurons'])+out)
+        
+        ### circles
+        for m in range(n):
+            color = "limegreen" if color == "green" else color
+            circle = plt.Circle(xy=(left+i*x_space, top_on_layer-m*y_space-4*p), radius=y_space/4.0, color=color, ec='k', zorder=4)
+            ax.add_artist(circle)
+            
+            ### add text
+            if i == 0:
+                plt.text(x=left-4*p, y=top_on_layer-m*y_space-4*p, fontsize=10, s=r'$X_{'+str(m+1)+'}$')
+            elif i == len(layer_sizes)-1:
+                plt.text(x=right+4*p, y=top_on_layer-m*y_space-4*p, fontsize=10, s=r'$y_{'+str(m+1)+'}$')
+            else:
+                plt.text(x=left+i*x_space+p, y=top_on_layer-m*y_space+(y_space/8.+0.01*y_space)-4*p, fontsize=10, s=r'$H_{'+str(m+1)+'}$')
+    
+    ## links
+    for i, (n_a, n_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+        layer = lst_layers[i+1]
+        color = "green" if i == len(layer_sizes)-2 else "blue"
+        color = "red" if layer['neurons'] == 0 else color
+        layer_top_a = y_space*(n_a-1)/2. + (top+bottom)/2. -4*p
+        layer_top_b = y_space*(n_b-1)/2. + (top+bottom)/2. -4*p
+        for m in range(n_a):
+            for o in range(n_b):
+                line = plt.Line2D([i*x_space+left, (i+1)*x_space+left], 
+                                  [layer_top_a-m*y_space, layer_top_b-o*y_space], 
+                                  c=color, alpha=0.5)
+                if layer['activation'] is None:
+                    if o == m:
+                        ax.add_artist(line)
+                else:
+                    ax.add_artist(line)
+    plt.show()
